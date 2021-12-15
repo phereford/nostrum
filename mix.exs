3d273671f51d839eedac4d6e52ba9cf70720ac01
Patrick Hereford <phereford@users.noreply.github.com>
2021-12-14 22:01:27 -0500

    upgrade mime

--- mix.exs
+++ mix.exs
@@ -102,7 +102,7 @@ defmodule Nostrum.Mixfile do
       {:certifi, "~> 2.8"},
       {:kcl, "~> 1.4"},
       {:porcelain, "~> 2.0"},
-      {:mime, "~> 1.6"},
+      {:mime, "~> 2.0"},
       {:ex_doc, "~> 0.15", only: :dev},
       {:credo, "~> 1.4", only: [:dev, :test]},
       {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
